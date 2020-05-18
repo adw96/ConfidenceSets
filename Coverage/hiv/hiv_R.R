3fe65297630881fd3ecf2b46347e528313d14c1b
Amy Willis <amy.willis7@gmail.com>
2020-05-18 15:05:35 -0700

    add TODO to HIV scripts

--- Coverage/hiv/hiv_R.R
+++ Coverage/hiv/hiv_R.R
@@ -1,3 +1,5 @@
+# TODO(AW) make this reproducible!
+
 ## a script to investigate the coverage of my confidence set procedure
 ## HIV dataset
 
@@ -8,7 +10,7 @@ setwd("Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/Coverage/"
 source("/Users/adw96/Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/miscellaneous_phylogenetics_programs.R")
 
 ################################################
-#################### n=20 
+#################### n=20
 ################################################
 trees <- list.files(path = "/Users/adw96/Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/Coverage/coverage_hiv/generated20/split/trees/")
 length(trees) ## should be 20000
@@ -36,11 +38,11 @@ for (batchNumber in 1:batches) {
     tree1 <- as.character(read.table(path)[1,1])
     write.table(convert_tree(tree1), paste("/Users/adw96/Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/Coverage/generated/test1.txt"), row.names = F, quote = F, col.names = F, append = T)
   }
-  
+
   ### run the script to get the log maps, and load them
   system('java -jar coverage_java.jar')
   source("/Users/adw96/Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/Coverage/generated/maps.R")
-  
+
   ### get the log map of the true tree
   system('java -jar coverage_truth_java.jar')
   source("/Users/adw96/Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/Coverage/generated/map_truth.R")
@@ -62,13 +64,13 @@ for (batchNumber in 1:batches) {
 }
 n=batchSize; m=length(tree1coveragesim)
 c(mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.90, m, n-m)),
-  mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)), 
+  mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)),
   mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.99, m, n-m)),
   mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.999, m, n-m)))
 containsTruth_HIV_20 <- containsTruth
 
 ################################################
-#################### n=50 
+#################### n=50
 ################################################
 trees <- list.files(path = "/Users/adw96/Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/Coverage/coverage_hiv/generated50/split/trees/")
 length(trees) ## should be 50000
@@ -108,13 +110,13 @@ for (batchNumber in 1:batches) {
 }
 n=batchSize; m=length(tree1coveragesim)
 c(mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.90, m, n-m)),
-  mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)), 
+  mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)),
   mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.99, m, n-m)),
   mean(containsTruth < (m*(n-1))/(n*(n-m))*qf(0.999, m, n-m)))
 containsTruth_HIV_50 <- containsTruth
 
 ################################################
-#################### n=100 
+#################### n=100
 ################################################
 trees <- list.files(path = "/Users/adw96/Documents/Phylogenetics/Tree Inference/Tree Inference Analysis/Coverage/coverage_hiv/generated100/split/trees/")
 length(trees) ## should be 100000
@@ -159,17 +161,17 @@ containsTruth_HIV_100 <- containsTruth
 m=2
 n=20
 100*c(mean(containsTruth_HIV_20 < (m*(n-1))/(n*(n-m))*qf(0.90, m, n-m)),
-  mean(containsTruth_HIV_20 < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)), 
+  mean(containsTruth_HIV_20 < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)),
   mean(containsTruth_HIV_20 < (m*(n-1))/(n*(n-m))*qf(0.99, m, n-m)),
   mean(containsTruth_HIV_20 < (m*(n-1))/(n*(n-m))*qf(0.999, m, n-m)))
 n=50
 100*c(mean(containsTruth_HIV_50 < (m*(n-1))/(n*(n-m))*qf(0.90, m, n-m)),
-  mean(containsTruth_HIV_50 < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)), 
+  mean(containsTruth_HIV_50 < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)),
   mean(containsTruth_HIV_50 < (m*(n-1))/(n*(n-m))*qf(0.99, m, n-m)),
   mean(containsTruth_HIV_50 < (m*(n-1))/(n*(n-m))*qf(0.999, m, n-m)))
 n=100
 100*c(mean(containsTruth_HIV_100 < (m*(n-1))/(n*(n-m))*qf(0.90, m, n-m)),
-  mean(containsTruth_HIV_100 < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)), 
+  mean(containsTruth_HIV_100 < (m*(n-1))/(n*(n-m))*qf(0.95, m, n-m)),
   mean(containsTruth_HIV_100 < (m*(n-1))/(n*(n-m))*qf(0.99, m, n-m)),
   mean(containsTruth_HIV_100 < (m*(n-1))/(n*(n-m))*qf(0.999, m, n-m)))
 
@@ -184,9 +186,9 @@ n=100
 # points(observations)
 # points(xbar[1], xbar[2], pch=16)
 # points(mu0[1], mu0[2], col="red")
-# grid1 <- cbind(rep(seq(min(observations[,1]), max(observations[,1]), length=100), each=100), 
+# grid1 <- cbind(rep(seq(min(observations[,1]), max(observations[,1]), length=100), each=100),
 #                rep(seq(min(observations[,2]), max(observations[,2]), length=100), 100))
-# grid1 <- cbind(rep(seq(-lim1, lim1, length=100), each=100), 
+# grid1 <- cbind(rep(seq(-lim1, lim1, length=100), each=100),
 #                rep(seq(-lim1, lim1, length=100), 100))
 # for (i in 1:dim(grid1)[1]) {
 #   tmp <- t(xbar - grid1[i, ])%*%solve(S)%*%(xbar - grid1[i, ])
@@ -195,9 +197,9 @@ n=100
 # }
 # ## conf int way too large
 # sqrt(m*(n-1)*qf(alpha, m, n-m)/(n*(n-m)))
-# 
+#
 # dim((xbar - grid1)%*%solve(S)%*%t(xbar - grid1))
-# 
+#
 # points(grid1)
 # ###
 # plot(pts, cex=0.5, pch=16)
